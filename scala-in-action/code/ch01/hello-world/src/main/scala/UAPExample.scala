feb8a9c59250f4da263f49d1799ce1592b983f95
keer2345 <keer2345@gmail.com>
2019-11-18 21:51:09 +0800

    scala-in-action/ch01.md

---
+++ scala-in-action/code/ch01/hello-world/src/main/scala/UAPExample.scala
@@ -0,0 +1,11 @@
+class UAPExample {
+  val someField = "hi"
+
+  def someMethod = "there"
+}
+
+object UAPExample extends App {
+  val o = new UAPExample
+  println(o.someField)
+  println(o.someMethod)
+}
